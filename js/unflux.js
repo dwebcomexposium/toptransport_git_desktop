65a498af4411114fb974362f55f4c3071ce58183
Aurélien Ravier <aurelien.ravier@unflux.fr>
2019-09-13 19:31:58 +0200

    Create unflux.js

---
+++ js/unflux.js
@@ -0,0 +1,5 @@
+$(function() {
+
+  $('.article-wrapper .article-title .inside .at-illust').after('<div class="img-mask" style="top: 60%;width: 100%;height: 100%;position: absolute;left: 0px;background-color: white;z-index: -1;pointer-events: none;">a</div>');
+
+});
