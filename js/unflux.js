8f583af016c0429f94bfab27fc9f8907715511fb
Aurélien Ravier <aurelien.ravier@unflux.fr>
2019-10-14 12:22:14 +0200

    Update unflux.js

--- js/unflux.js
+++ js/unflux.js
@@ -1,6 +1,6 @@
 $(function() {
 
-  $('.article-wrapper .article-title .inside .at-illust').after('<div class="img-mask" style="top: 60%;width: 100%;height: 100%;position: absolute;left: 0px;background-color: white;z-index: -1;pointer-events: none;">a</div>');
+  $('.article-wrapper .article-title .inside .at-illust').after('<div class="img-mask" style="top: 60%;width: 100%;height: 100%;position: absolute;left: 0px;background-color: white;z-index: -1;pointer-events: none;"></div>');
 
 //SLIDER TESTIMONIALS
 $('.slider').each(function() {
